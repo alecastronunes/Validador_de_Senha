66532034f65e17d312d5c876e2734447ca2b863a
Alexandre Castro <alexandre.castro92@gmail.com>
2025-01-08 18:58:42 -0300

    Subindo a versão finalizada do projeto

---
+++ readme.md
@@ -0,0 +1,22 @@
+# WEB - Validador de Senha📦
+
+
+## Objetivo:  📝
+
+O objetivo principal do sistema é realizar a validação de senha com base em 4 verificações preestabelecidas:
+
+1. Pelo menos um caractere minúsculo e um maiúsculo
+2. Pelo menos um número
+3. Pelo menos um caractere especial (!%&@#$*?_)
+4. Pelo menos 6 caracteres.
+
+Ao passo em que cada verificação é satisfeita, o ícone referente a cada regra que está em vermelho, é alterado para verde e a barra de progresso é atualizada, até que por fim, todas as regras sejam satisfeitas, a senha validada e a barra de progresso finalizada.
+
+
+## Tecnologias utilizadas:  📝
+
+HTML;
+
+CSS;
+
+Javascript.
---
+++ script/script.js
@@ -0,0 +1,48 @@
+let passwordInput = document.querySelector(".password");
+let progressBar = document.querySelector(".progress .bar");
+
+passwordInput.addEventListener("keyup", () => {
+  let strength = 0;
+
+  //  Verifica se a senha contém letras maiúsculas e minúsculas
+  strength += renderRule(
+    ".low-upper-case",
+    passwordInput.value.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)
+  );
+
+  //  Verifica se a senha contém números
+  strength += renderRule(".one-numer", passwordInput.value.match(/([0-9])/));
+
+  //  Verifica se a senha contém caracteres especiais
+  strength += renderRule(
+    ".one-especial-char",
+    passwordInput.value.match(/[!@#$%^&*(),.?":{}|<>_+-]/)
+  );
+
+  //  Verifica se a senha tem pelo menos 6 caracteres
+  strength += renderRule(".min-char", passwordInput.value.length >= 6);
+
+  //   Constrói a barra de progresso
+  let percent = Math.floor((strength / 4) * 100);
+  progressBar.style.width = `${percent}%`;
+
+  if(percent === 100) {
+    alert("Senha verificada com sucesso!!!")
+  }
+
+  //   if (passwordInput.value.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+  //     // strength += 1;
+  //     renderRule(".low-upper-case", true);
+  //   } else {
+  //     renderRule(".low-upper-case", false);
+  //   }
+});
+
+function renderRule(ruleClass, valid) {
+  let ruleArea = document.querySelector(ruleClass);
+  ruleArea.querySelector(".valid-point").style.backgroundColor = valid
+    ? "green"
+    : "red";
+
+  return valid ? 1 : 0;
+}
